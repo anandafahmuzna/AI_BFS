3d26b387c8bda321214c09090cc575e00671635f
anandafahmuzna <nandafmznaa@gmail.com>
2025-06-19 18:49:50 +0700

    Add files via upload

---
+++ TA_BFS.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+def bfs(graph, start, goal):
+    queue = deque([[start]])
+    visited = set()
+
+    while queue:
+        path = queue.popleft()
+        node = path[-1]
+
+        if node == goal:
+            return path
+
+        if node not in visited:
+            visited.add(node)
+            for neighbor in graph.get(node, []):
+                new_path = list(path)
+                new_path.append(neighbor)
+                queue.append(new_path)
+    
+    return None
+
+#Representasi lingkungan sebagai graf
+graph = {
+    'Rumah': ['Warung', 'Gang Belakang'],
+    'Warung': ['Rumah', 'Taman'],
+    'Gang Belakang': ['Rumah', 'Taman'],
+    'Taman': ['Warung', 'Gang Belakang', 'Sekolah'],
+    'Sekolah': ['Taman']
+}
+
+start_node = 'Rumah'
+goal_node = 'Sekolah'
+
+result = bfs(graph, start_node, goal_node)
+
+if result:
+    print("Jalur tercepat ditemukan:")
+    print(" -> ".join(result))
+else:
+    print("Jalur tidak ditemukan.")
